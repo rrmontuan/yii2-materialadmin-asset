6b9a7a45fd0844e2077f6fd03ef48cba5f5fdb69
Ricardo Montuan <ricardo.montuan@hotmail.com>
2018-05-21 20:32:40 -0300

    v1.0.2

--- web/MaterialAdminAsset.php
+++ web/MaterialAdminAsset.php
@@ -15,18 +15,20 @@ class MaterialAdminAsset extends BaseMaterialAdminLteAsset
     	'vendors/animate-css/animate.min.css',
 		'vendors/sweet-alert/sweet-alert.min.css',
 		'vendors/material-icons/material-design-iconic-font.min.css',
-		'vendors/socicon/socicon.min.css','css/app.min.1.css',
+		'vendors/socicon/socicon.min.css',
+		'css/app.min.1.css',
 		'css/app.min.2.css'
     ];
 		
     public $js = [
-    	//"js/jquery-2.1.1.min.js",
-    	//"js/bootstrap.min.js",
         'vendors/nicescroll/jquery.nicescroll.min.js',
         'vendors/waves/waves.min.js',
-        'vendors/sweet-alert/sweet-alert.min.js','js/charts.js',
-        'js/functions.js',
-        //'js/demo.js',
+        'vendors/sweet-alert/sweet-alert.min.js',
+		'js/charts.js',
+        'vendors/bootstrap-select/bootstrap-select.min.js',
+    	'vendors/moment/moment.min.js',
+    	'vendors/sparklines/jquery.sparkline.min.js',
+    	'js/functions.js',
     ];
 	
     public $depends = [
